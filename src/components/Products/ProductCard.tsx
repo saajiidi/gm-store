518b8251ee833c6a03730c6f265f0008022791fc
Sajid al-Islam <saajiidi@gmail.com>
2024-06-12 00:07:03 +0600

    product listing update

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaBoxOpen } from "react-icons/fa";
 
 interface Product {
   id: number;
@@ -15,22 +16,54 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const discountPercentage = Math.floor(
+    ((product.originalPrice - product.discountedPrice) /
+      product.originalPrice) *
+      100
+  );
+
   return (
     <div className="col-md-4 mb-4">
-      <div className="card h-100">
-        <Link to={`/product/${product.id}`}>
-          <img src={product.image} alt={product.name} className="card-img-top" />
+      <div className="card h-100 w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+        <span>
+          <s className="absolute top-2 left-2 text-left text-red-500 text-xl">
+            &#2547;{product.originalPrice.toFixed(2)}
+          </s>
+          <p className="absolute top-2 right-2 text-left text-green-500 text-xl">
+            &#2547;{product.discountedPrice.toFixed(2)}
+          </p>
+        </span>
+        <Link to={`/product/${product.id}`} className="block relative">
+          <div className="flex justify-center items-center h-60">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="card-img-top h-full object-cover"
+            />
+            <span className="absolute top-2 right-2 bg-orange-500 text-white font-bold py-2 px-4 rounded-full">
+              -{discountPercentage}%
+            </span>
+            <span className="absolute bottom-2 left-2 bg-green-500 text-white py-2 px-4 rounded-full">
+              <FaBoxOpen className="mr-2" /> {product.quantity}
+            </span>
+          </div>
         </Link>
         <div className="card-body">
-          <Link to={`/product/${product.id}`}>
+          <Link
+            to={`/product/${product.id}`}
+            className="no-underline text-chocolate font-bold block text-center"
+          >
             <h5 className="card-title">{product.name}</h5>
           </Link>
-          <p className="card-text">
-            Original Price: ${product.originalPrice.toFixed(2)}
-          </p>
-          <p className="card-text">
-            Discounted Price: ${product.discountedPrice.toFixed(2)}
-          </p>
+
+          <div className="flex justify-between mt-4 space-x-2">
+            <button className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Add to Cart
+            </button>
+            <button className="w-1/2 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full">
+              Buy Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Products/ProductListing.tsx
+++ src/components/Products/ProductListing.tsx
@@ -1,62 +1,69 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import helmet1 from "../../assets/img/helmet1.jpg";
+import helmet2 from "../../assets/img/helmet2.jpg";
+import light1 from "../../assets/img/light1.jpg";
+import light2 from "../../assets/img/light2.jpg";
+import lock1 from "../../assets/img/lock1.jpg";
+import lock2 from "../../assets/img/lock2.jpg";
 
 const products = [
   {
     id: 1,
     name: "Bike Helmet",
-    image: "https://via.placeholder.com/300x300?text=Bike+Helmet",
-    originalPrice: 120.0,
-    discountedPrice: 100.0,
+    image: helmet1,
+    originalPrice: 3000.0,
+    discountedPrice: 2500.0,
     quantity: 10,
   },
   {
     id: 2,
     name: "Bike Light",
-    image: "https://via.placeholder.com/300x300?text=Bike+Light",
-    originalPrice: 60.0,
-    discountedPrice: 50.0,
+    image: light1,
+    originalPrice: 300.0,
+    discountedPrice: 200.0,
     quantity: 15,
   },
   {
     id: 3,
     name: "Bike Lock",
-    image: "https://via.placeholder.com/300x300?text=Bike+Lock",
-    originalPrice: 40.0,
-    discountedPrice: 30.0,
+    image: lock1,
+    originalPrice: 150.0,
+    discountedPrice: 120.0,
     quantity: 20,
   },
   {
     id: 4,
     name: "Bike Helmet",
-    image: "https://via.placeholder.com/300x300?text=Bike+Helmet",
-    originalPrice: 120.0,
-    discountedPrice: 100.0,
+    image: helmet2,
+    originalPrice: 5000.0,
+    discountedPrice: 4000.0,
     quantity: 10,
   },
   {
     id: 5,
     name: "Bike Light",
-    image: "https://via.placeholder.com/300x300?text=Bike+Light",
-    originalPrice: 60.0,
-    discountedPrice: 50.0,
+    image: light2,
+    originalPrice: 1500.0,
+    discountedPrice: 1200.0,
     quantity: 15,
   },
   {
     id: 6,
     name: "Bike Lock",
-    image: "https://via.placeholder.com/300x300?text=Bike+Lock",
-    originalPrice: 40.0,
-    discountedPrice: 30.0,
+    image: lock2,
+    originalPrice: 500.0,
+    discountedPrice: 400.0,
     quantity: 20,
   },
+]; 
   // Add more products as needed
-];
+
 
 const ProductListing: React.FC = () => {
   return (
-    <div className="container mt-5">
-      <div className="row">
+    <div className="container mt-5 ">
+      <div className="row ">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
